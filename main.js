4fd1688119748e3d5020f5f77b67ce6a86046954
Liam J. <liam@jewer.ca>
2023-09-27 16:59:58 -0400

    updated javascript for links

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var toggle = true;
 function clicked() {
     document.getElementsByClassName("nav-icon")[0].classList.toggle("open");
     document.getElementsByClassName("circle")[0].classList.toggle("bounce-left");
-    document.getElementsByClassName("circle")[1].classList.toggle("bounce");
-    document.getElementsByClassName("circle")[2].classList.toggle("bounce-right");
+    //document.getElementsByClassName("circle")[1].classList.toggle("bounce");
+    document.getElementsByClassName("circle")[1].classList.toggle("bounce-right");
     if (toggle) {
         document.getElementById("nav-container").style.transform = "rotateX(0deg)";
 
@@ -16,9 +16,9 @@ function clicked() {
 
 function clickedMobile() {
     document.getElementsByClassName("nav-icon")[0].classList.toggle("open");
-    document.getElementsByClassName("section")[0].classList.toggle("bounce");
-    document.getElementsByClassName("section")[1].classList.toggle("bounce-left");
-    document.getElementsByClassName("section")[2].classList.toggle("bounce-right");
+    //document.getElementsByClassName("section")[0].classList.toggle("bounce");
+    document.getElementsByClassName("section")[0].classList.toggle("bounce-left");
+    document.getElementsByClassName("section")[1].classList.toggle("bounce-right");
     if (toggle) {
         document.getElementById("mobinav").style.transform = "rotateX(0deg)";
 
